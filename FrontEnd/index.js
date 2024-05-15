8f92eba6e310fc32dd2e87a54cd2b86a9e566311
phongpanphokaew <s6206021631029@emailkmutnb.ac.th>
2024-05-15 23:48:35 +0700

    up

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -2,11 +2,25 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT||5500;
+const port = process.env.PORT||3000;
 
-app.get("/",(req,res) => {
-    res.send("Hello World! Phongpan");
-});
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+  });
+
+  app.get("/location", (req, res) => {
+    res.render("location");
+  });
+
+  app.get("/Photo", (req, res) => {
+    res.render("Photo");
+  });
+
+  app.get("/Photographer", (req, res) => {
+    res.render("Photographer");
+  });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
